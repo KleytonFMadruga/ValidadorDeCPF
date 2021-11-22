14df29e9b03a61fc4a96eb3be7a7035727e85ee2
KleytonFMadruga <kleyton.fernandes@dcx.ufpb.br>
2021-11-21 23:04:20 -0300

    inicio de calculo do primeiro digito verificador

--- src/com/kleyton/Main.java
+++ src/com/kleyton/Main.java
@@ -2,6 +2,7 @@ package src.com.kleyton;
 public class Main{
     
     public static void main(String[] args) {
-        
+       
+        Validador.validarCPF("529.982.247-25");
     }
 }
--- src/com/kleyton/Validador.java
+++ src/com/kleyton/Validador.java
@@ -1,11 +1,19 @@
 package src.com.kleyton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.PopupFactory;
+
 public class Validador{
 
     public static boolean validarCPF(String cpf){
-        String resultado = cpf.replace("-", "");
-        resultado = resultado.replace(".", "");
-        System.out.println(resultado);
+        String cpfLimpo = cpf.replace("-", "");
+        cpfLimpo = cpfLimpo.replace(".", "");
+        char[] listaCaractereCPF = cpfLimpo.toCharArray();
+        List <Integer> listaNumeroCPF = new ArrayList<Integer>();
+        for(char caractere : listaCaractereCPF) listaNumeroCPF.add(Integer.parseInt(String.valueOf(caractere)));
+        listaNumeroCPF.stream().forEach(System.out::println);
         return true;
     }
 
